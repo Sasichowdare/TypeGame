0d090ba73f5ec2671110213d42cb732d78bb54af
Sasichowdare <sasidhar44292@gmail.com>
2025-03-12 14:34:40 +0530

    modifed db.config

--- server/models/db.js
+++ server/models/db.js
@@ -1,12 +1,24 @@
+// const { Pool } = require('pg');
+
+// const pool = new Pool({
+//   user: 'postgres',
+//   //host: 'localhost',
+//   host:"typegame-db-1",
+//   database: 'TypeGame',
+//   password: 'sasi',
+//   port: 5432,
+// });
+
+// module.exports = pool;
+
 const { Pool } = require('pg');
 
 const pool = new Pool({
   user: 'postgres',
-  //host: 'localhost',
-  host:"typegame-db-1",
-  database: 'TypeGame',
-  password: 'sasi',
-  port: 5432,
+  host: 'postgres.railway.internal', // Use the internal Railway host
+  database: 'railway', // Ensure this is your actual database name, e.g., 'railway'
+  password: 'DTErhoTwlhETBpJlrxGPdNJYpTTaaLut', // Your password
+  port: 5432, // Default PostgreSQL port
 });
 
 module.exports = pool;
